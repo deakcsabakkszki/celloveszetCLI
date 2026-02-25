e95c8ee714e7ecd93096ef04b7b09ad8d7860e7a
qgedeak@gmail.com <qgedeak@gmail.com>
2026-02-25 11:03:36 +0100

    KRB

--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] adatok = File.ReadAllLines("lovesek.csv");
+            List<CelLovo> celLovok = new List<CelLovo>();
+
+            foreach (var item in adatok)
+            {
+                celLovok.Add(new CelLovo(item));
+            }
+
+
         }
     }
 }
